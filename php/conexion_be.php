bef4eece6c4b6093f3dede8c1be0a3033beacc1f
paulaminguez14 <dominguez.bernabe.paula@iescamas.es>
2024-12-02 20:01:43 +0100

    Update conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,11 +1,10 @@
 <?php
-
-$conexion = mysqli_connect("localhost", "root", "", "loginavanzado");
-
-if ($conexion){
-    echo "conectado";
-}else{
-        echo "no conectado";
+$host = "localhost"; // O la dirección del servidor MySQL de CDMON.
+$user = "nombre_usuario"; // Tu usuario MySQL.
+$password = "contraseña"; // Tu contraseña MySQL.
+$db = "loginavanzado"; // El nombre de la base de datos creada.
+$conn = new mysqli($host, $user, $password, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
